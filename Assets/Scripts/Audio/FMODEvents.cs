275c3d5be0f087d71bdd2d0c617a1d8eaccd2547
Syntoca <rileykkmusic@Gmail.com>
2023-11-16 15:08:15 -0800

    Bug Death Added
    Bug death added

---
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference music { get; private set; }
+   public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events script in the scene.");
+        }
+        instance = this;
+    }
+}
